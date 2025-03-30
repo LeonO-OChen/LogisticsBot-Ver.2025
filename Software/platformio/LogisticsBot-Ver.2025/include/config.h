05965df8f97260caf9cee61f95af221bfac636b3
Leon.ch <leono_o@163.com>
2025-03-30 15:32:20 +0800

    update

--- Software/platformio/LogisticsBot-Ver.2025/include/config.h
+++ Software/platformio/LogisticsBot-Ver.2025/include/config.h
@@ -1,15 +1,61 @@
 
 
-#define PIN_LED 2
-
 // I2C
 #define I2C_SCL 40
 #define I2C_SDA 39
 #define I2C_FREQ 200000
 
+// LED 显示工作状态
+#define PIN_LED_D 42
+#define PIN_LED_CLK 41
+#define PIN_LED_LOAD 38
+
+// 巡迹模块引脚
+/*
+   1 2 3 4
+9          12
+10         13
+11         14
+   5 6 7 8
+*/
+#define PIN_XUNJI_1 7
+#define PIN_XUNJI_2 15
+#define PIN_XUNJI_3 16
+#define PIN_XUNJI_4 5
+
+#define PIN_XUNJI_5 4
+#define PIN_XUNJI_6 12
+#define PIN_XUNJI_7 13
+#define PIN_XUNJI_8 14
+
+#define PIN_XUNJI_9 8
+#define PIN_XUNJI_10 18
+#define PIN_XUNJI_11 17
+
+#define PIN_XUNJI_12 11
+#define PIN_XUNJI_13 10
+#define PIN_XUNJI_14 9
 
 
-#define PIN_LED_A0 15
-#define PIN_LED_A1 14
-#define PIN_LED_A2 13
+// 微动开关
+// 1 2
+#define PIN_WEIDONG_1 47
+#define PIN_WEIDONG_2 21
 
+// 旋转编码开关
+#define PIN_CODE_CLK 35   // 时钟
+#define PIN_CODE_D 36     // 数字输入
+#define PIN_CODE_SW 37    // 开关
+
+// 光敏电阻引脚
+
+#define PIN_GUANGMIN_1 1  // 白
+#define PIN_GUANGMIN_2 2  // 黄
+#define PIN_GUANGMIN_3 6   // 辨色
+
+// 风扇
+#define PIN_FAN_1 19   // 风扇1
+#define PIN_FAN_2 20   // 风扇2
+
+#define VEHICLE_SHEELBASE  27     // 轴距(cm)
+#define VEHICLE_TREAD      26     // 轮距(cm)
--- Software/platformio/LogisticsBot-Ver.2025/src/main.cpp
+++ Software/platformio/LogisticsBot-Ver.2025/src/main.cpp
@@ -2,46 +2,160 @@
   CPU:  ESP32-S3
 */
 
-#include "config.h"
 #include "MSDriverMaster.h"
+#include "common.h"
+#include "config.h"
 #include "i2cMaster.h" // I2C通信
-
 
 I2C_Master _i2c;
 
 // 电机驱动模块
 MSDriverMaster _MSDriverMaster;
+int _Task = 0;            // 任务（1:排空 2：装货 3~6:送货 7:测试1 8:测试2）
+volatile int _Select = 1; // 选择项 1~8  (使用volatile，因为会在中断中修改)
 
 void initMSDriver();
-void showLed(uint8_t val);
+void showLed(int val);
+void handleEncoder();
+int buttonDown();
 
 void setup() {
-    Serial.begin(115200); // 初始化串口，波特率设置为9600
-    // _i2c.init(I2C_SDA, I2C_SCL, I2C_FREQ);
-    pinMode(PIN_LED, OUTPUT);
-    pinMode(PIN_LED_A0, OUTPUT);
-    pinMode(PIN_LED_A1, OUTPUT);
-    pinMode(PIN_LED_A2, OUTPUT);
+    Serial.begin(115200); // 初始化串口
+    _i2c.init(I2C_SDA, I2C_SCL, I2C_FREQ);
+
+    // 数码管
+    pinMode(PIN_LED_D, OUTPUT);
+    pinMode(PIN_LED_CLK, OUTPUT);
+    pinMode(PIN_LED_LOAD, OUTPUT);
+
+    // 旋转编码器
+    pinMode(PIN_CODE_CLK, INPUT);       // 时钟
+    pinMode(PIN_CODE_D, INPUT);         // 数据
+    pinMode(PIN_CODE_SW, INPUT_PULLUP); // 按钮
+    // 设置外部中断
+    attachInterrupt(PIN_CODE_CLK, handleEncoder, CHANGE);
 
     // 等待其它设备上电完毕
     delay(1000);
 
-    //initMSDriver();
+    initMSDriver();
 
     delay(1000);
+    _MSDriverMaster.motor(0, 100);
+    _MSDriverMaster.motor(1, 100);
+    _MSDriverMaster.motor(2, 100);
+    _MSDriverMaster.motor(3, 100);
 }
 
 // 主任务
 void loop() {
-    for (int i = 0; i < 8; i++) {
-        showLed(i);
-        delay(500);
+
+    static int ledFlash = 0;
+    static unsigned long t0 = 0;
+
+    int offset; // 旋转编码器读取结果
+    // 读取选择编码器按钮
+    // 选中的选项作为下一个任务
+    if (buttonDown()) {
+        _Task = _Select;
     }
+
+    // 之后的程序每50ms才运行一次
+    if (!timePassed(t0, 10)) {
+        return;
+    }
+
+    // readXunJiChuanGanQi();
+    // readWeiDongKaiGuan();
+
+    if (_Task == 0) {
+        // 不执行任务时： 指示灯闪烁
+        ledFlash = 1 - ledFlash;
+        if (ledFlash) {
+            showLed(_Select);
+        } else {
+            showLed(-1);
+        }
+    } else {
+        // 执行任务时： 指示灯常亮
+        showLed(_Select);
+    }
+    if (_Task) {
+        delay(2000);
+        _Task = 0;
+    }
+
+    // if (_Task == 1) { // 排空
+    //     displayMode = 0;
+    //     empty();
+    //     _Task = 0;
+    // } else if (_Task == 2) { // 装货
+    //     displayMode = 0;
+    //     renwu1();
+
+    //     if (digitalRead(PIN_MODE_TEST)) {
+    //         _Task = 0;
+    //     } else {
+    //         _Task++;
+    //     }
+
+    // } else if (_Task == 3) { // 起点送货
+    //     displayMode = 0;
+    //     renwu2_1();
+    //     if (digitalRead(PIN_MODE_TEST)) {
+    //         _Task = 0;
+    //     } else {
+    //         _Task++;
+    //     }
+    //     _Select = _Task;
+    // } else if (_Task == 4) { // 中间1送货
+    //     displayMode = 0;
+    //     renwu2_2();
+    //     if (digitalRead(PIN_MODE_TEST)) {
+    //         _Task = 0;
+    //     } else {
+    //         _Task++;
+    //     }
+    //     _Select = _Task;
+    // } else if (_Task == 5) { // 中间2送货
+    //     displayMode = 0;
+    //     renwu3_1();
+    //     if (digitalRead(PIN_MODE_TEST)) {
+    //         _Task = 0;
+    //     } else {
+    //         _Task++;
+    //     }
+    //     _Select = _Task;
+    // } else if (_Task == 6) { // 中间3送货
+    //     displayMode = 0;
+    //     renwu3_2();
+    //     _Task = 0;
+    //     _Select = _Task;
+    // } else if (_Task == 7) { // 测试
+    //     displayMode = 0;
+    //     Test1();
+
+    //     displayMode = 0;
+    //     sleep(20);
+    //     oled.clearDisplay(); // 清屏
+    //     oled.display();
+    //     _Task = 0;
+
+    // } else if (_Task == 8) { // 测试2
+    //     displayMode = 0;
+    //     Test8();
+    //     displayMode = 0;
+    //     sleep(20);
+    //     oled.clearDisplay(); // 清屏
+    //     oled.display();
+    //     _Task = 0;
+    // }
 }
 
 // 4电机闭环控制，舵机全开
 void initMSDriver() {
-    uint8_t motorMode = 0b11001001; // 测速，计数自动清零，PID控制
+    // uint8_t motorMode = 0b11000001; // 测速，计数自动清零，PID控制
+    uint8_t motorMode = 0b11000000; // 测速，计数自动清零，无PID控制
 
     uint8_t smode = 0b11; // 舵机模式
     _MSDriverMaster.init(0x32);
@@ -51,8 +165,88 @@ void initMSDriver() {
     _MSDriverMaster.sendCmd(APPLY);
 }
 
-void showLed(uint8_t val) {
-    digitalWrite(PIN_LED_A0, val& 0b0001);
-    digitalWrite(PIN_LED_A1, val& 0b0010);
-    digitalWrite(PIN_LED_A2, val& 0b0100);
+unsigned char smgduan[] = {
+    // 共阳
+    B11000000, // 0
+    B11111001, // 1
+    B10100100, // 2
+    B10110000, // 3
+    B10011001, // 4
+    B10010010, // 5
+    B10000010, // 6
+    B11111000, // 7
+    B10000000, // 8
+    B10010000, // 9
+    B01000000, // 0.
+    B01111001, // 1.
+    B00100100, // 2.
+    B00110000, // 3.
+    B00011001, // 4.
+    B00010010, // 5.
+    B00000010, // 6.
+    B01111000, // 7.
+    B00000000, // 8.
+    B00010000, // 9.
+    B11111111, // 空
+}; // 显示0~9的值共阴
+
+void showLed(int val) {
+
+    unsigned char gewei = 20;
+    unsigned char shiwei = 20;
+    if (val >= 0) {
+        gewei = (val % 100) % 10;
+        shiwei = (val % 100) / 10;
+    }
+
+    digitalWrite(PIN_LED_LOAD, LOW); // 低电位表示启动
+    shiftOut(PIN_LED_D, PIN_LED_CLK, MSBFIRST, smgduan[gewei]);
+    shiftOut(PIN_LED_D, PIN_LED_CLK, MSBFIRST, smgduan[shiwei]);
+    digitalWrite(PIN_LED_LOAD, HIGH); // 高电位表示停止
+}
+
+void handleEncoder() {
+    // 防抖动变量
+    static unsigned long lastDebounceTime = 0;
+    static unsigned long debounceDelay = 1000; // 微秒
+    static int position = 0;
+
+    // 如果正在执行任务，则不读取
+    if (_Task) {
+        return;
+    }
+
+    // 防抖动处理
+    if ((micros() - lastDebounceTime) < debounceDelay) {
+        return;
+    }
+    lastDebounceTime = micros();
+
+    // 读取两个引脚的状态
+    int a = digitalRead(PIN_CODE_CLK);
+    int b = digitalRead(PIN_CODE_D);
+
+    // 根据A、B相的状态变化判断方向
+    // 这里假设编码器是四分之一周期检测（每个变化都检测）
+    if (a == b) {
+        position--; // 顺时针
+    } else {
+        position++; // 逆时针
+    }
+    _Select = position / 2;
+}
+
+int buttonDown() {
+    // 旋转编码器输出判断用——记录前一次信号状态
+    static int previousOutputSW = LOW;
+    int codeSW = digitalRead(PIN_CODE_SW);
+
+    if (previousOutputSW == HIGH && codeSW == LOW) {
+        delay(50);
+        previousOutputSW = codeSW;
+        return 1;
+    }
+
+    previousOutputSW = codeSW;
+    return 0;
 }
